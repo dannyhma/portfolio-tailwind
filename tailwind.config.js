3ac3c6a68d792ad244c0dead52367aa94a8f3840
dannyhma <dannyhimawan089@gmail.com>
2023-10-29 15:01:38 +0700

    rename folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#06b6d4',
+        dark900: '#0f172a',
+        dark500: '#64748b',
+        dark400: '#94a3b8',
+      },
+    },
   },
   plugins: [],
 }
