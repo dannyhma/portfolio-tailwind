809ada06ead85840b7e4f701b2731a85f4fc40a8
dannyhma <dannyhimawan089@gmail.com>
2023-10-29 18:51:46 +0700

    update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: 16,
+    },
     extend: {
       colors: {
         primary: '#14b8a6',
@@ -9,6 +13,18 @@ module.exports = {
         dark900: '#0f172a',
         dark500: '#64748b',
         dark400: '#94a3b8',
+        whatsapp: '#25D366',
+        instagram: '#E4405F',
+        facebook: '#0866FF',
+        discord: '#5865F2',
+        linkedin: '#0A66C2',
+        figma: '#F24E1E',
+      },
+      scale: {
+        '140': '1.4',
+      },
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
